b4ab4b1dee7e6d3d773324bc4e8c2a4af6197ec4
yibo7 <58937236+yibo7@users.noreply.github.com>
2022-01-09 18:21:18 +0800

    my.in

--- SetupMySql/Main.Designer.cs
+++ SetupMySql/Main.Designer.cs
@@ -35,19 +35,18 @@
             this.label2 = new System.Windows.Forms.Label();
             this.lbPath = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pbSetup = new System.Windows.Forms.ProgressBar();
+            this.lb_del_mysql = new System.Windows.Forms.LinkLabel();
             this.label3 = new System.Windows.Forms.Label();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.btnSetPrivateKey = new System.Windows.Forms.Button();
-            this.txtPrivateKey = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnSetup
             // 
-            this.btnSetup.Location = new System.Drawing.Point(71, 184);
+            this.btnSetup.Location = new System.Drawing.Point(53, 147);
+            this.btnSetup.Margin = new System.Windows.Forms.Padding(2);
             this.btnSetup.Name = "btnSetup";
-            this.btnSetup.Size = new System.Drawing.Size(195, 59);
+            this.btnSetup.Size = new System.Drawing.Size(146, 47);
             this.btnSetup.TabIndex = 0;
             this.btnSetup.Text = "开始安装";
             this.btnSetup.UseVisualStyleBackColor = true;
@@ -56,47 +55,54 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(20, 76);
+            this.label1.Location = new System.Drawing.Point(15, 61);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.Size = new System.Drawing.Size(35, 12);
             this.label1.TabIndex = 1;
             this.label1.Text = "端口:";
             // 
             // txtPort
             // 
-            this.txtPort.Location = new System.Drawing.Point(71, 73);
+            this.txtPort.Location = new System.Drawing.Point(53, 58);
+            this.txtPort.Margin = new System.Windows.Forms.Padding(2);
             this.txtPort.Name = "txtPort";
-            this.txtPort.Size = new System.Drawing.Size(100, 25);
+            this.txtPort.Size = new System.Drawing.Size(76, 21);
             this.txtPort.TabIndex = 2;
             this.txtPort.Text = "3306";
             // 
             // txtPass
             // 
-            this.txtPass.Location = new System.Drawing.Point(71, 124);
+            this.txtPass.Location = new System.Drawing.Point(53, 99);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(2);
             this.txtPass.Name = "txtPass";
-            this.txtPass.Size = new System.Drawing.Size(313, 25);
+            this.txtPass.Size = new System.Drawing.Size(236, 21);
             this.txtPass.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(20, 127);
+            this.label2.Location = new System.Drawing.Point(15, 102);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.Size = new System.Drawing.Size(35, 12);
             this.label2.TabIndex = 3;
             this.label2.Text = "密码:";
             // 
             // lbPath
             // 
             this.lbPath.AutoSize = true;
-            this.lbPath.Location = new System.Drawing.Point(20, 30);
+            this.lbPath.Location = new System.Drawing.Point(15, 24);
+            this.lbPath.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbPath.Name = "lbPath";
-            this.lbPath.Size = new System.Drawing.Size(67, 15);
+            this.lbPath.Size = new System.Drawing.Size(53, 12);
             this.lbPath.TabIndex = 5;
             this.lbPath.Text = "当前目录";
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.pbSetup);
+            this.groupBox1.Controls.Add(this.lb_del_mysql);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.btnSetup);
             this.groupBox1.Controls.Add(this.lbPath);
@@ -104,64 +110,55 @@
             this.groupBox1.Controls.Add(this.txtPass);
             this.groupBox1.Controls.Add(this.txtPort);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Location = new System.Drawing.Point(12, 146);
+            this.groupBox1.Location = new System.Drawing.Point(11, 11);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(449, 313);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(357, 280);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "安装数据库";
             // 
+            // pbSetup
+            // 
+            this.pbSetup.Location = new System.Drawing.Point(7, 244);
+            this.pbSetup.Name = "pbSetup";
+            this.pbSetup.Size = new System.Drawing.Size(333, 23);
+            this.pbSetup.TabIndex = 8;
+            // 
+            // lb_del_mysql
+            // 
+            this.lb_del_mysql.AutoSize = true;
+            this.lb_del_mysql.Location = new System.Drawing.Point(249, 164);
+            this.lb_del_mysql.Name = "lb_del_mysql";
+            this.lb_del_mysql.Size = new System.Drawing.Size(59, 12);
+            this.lb_del_mysql.TabIndex = 7;
+            this.lb_del_mysql.TabStop = true;
+            this.lb_del_mysql.Text = "卸载MYSQL";
+            this.lb_del_mysql.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lb_del_mysql_LinkClicked);
+            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label3.Location = new System.Drawing.Point(7, 272);
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label3.Location = new System.Drawing.Point(5, 218);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(421, 15);
+            this.label3.Size = new System.Drawing.Size(335, 12);
             this.label3.TabIndex = 6;
             this.label3.Text = "注:安装前确认当前目录存在db的数据库源目录，且不要带中文";
             // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.btnSetPrivateKey);
-            this.groupBox2.Controls.Add(this.txtPrivateKey);
-            this.groupBox2.Location = new System.Drawing.Point(13, 24);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(448, 100);
-            this.groupBox2.TabIndex = 7;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "初始化秘钥";
-            // 
-            // btnSetPrivateKey
-            // 
-            this.btnSetPrivateKey.Location = new System.Drawing.Point(346, 37);
-            this.btnSetPrivateKey.Name = "btnSetPrivateKey";
-            this.btnSetPrivateKey.Size = new System.Drawing.Size(84, 26);
-            this.btnSetPrivateKey.TabIndex = 7;
-            this.btnSetPrivateKey.Text = "设置密钥";
-            this.btnSetPrivateKey.UseVisualStyleBackColor = true;
-            this.btnSetPrivateKey.Click += new System.EventHandler(this.btnSetPrivateKey_Click);
-            // 
-            // txtPrivateKey
-            // 
-            this.txtPrivateKey.Location = new System.Drawing.Point(22, 37);
-            this.txtPrivateKey.Name = "txtPrivateKey";
-            this.txtPrivateKey.Size = new System.Drawing.Size(313, 25);
-            this.txtPrivateKey.TabIndex = 6;
-            // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(482, 495);
-            this.Controls.Add(this.groupBox2);
+            this.ClientSize = new System.Drawing.Size(380, 302);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Main";
-            this.Text = "安装数据库";
+            this.Text = "MYSQL秒装器";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -175,10 +172,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lbPath;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button btnSetPrivateKey;
-        private System.Windows.Forms.TextBox txtPrivateKey;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel lb_del_mysql;
+        private System.Windows.Forms.ProgressBar pbSetup;
     }
 }
 
--- SetupMySql/Main.cs
+++ SetupMySql/Main.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,19 +20,12 @@ namespace SetupMySql
 {
     public partial class Main : Form
     {
-        private string mysqlSetting = @"
-[client]                                                                                                      
+        private string mysqlSetting = @"[client]                                                                                                      
 port=#端口#
 default-character-set=utf8
 [mysqld]
 port=#端口#
 character_set_server=utf8
-basedir=#数据库程序目录#
-datadir=#数据存储目录#
-sql_mode=NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES
-[WinMySQLAdmin]
-#服务路径#
-
 ";
         public Main()
         {
@@ -41,10 +37,8 @@ sql_mode=NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES
         }
 
         private void btnSetup_Click(object sender, EventArgs e)
-        {
-            
-
-            CmdHelper cmd = new CmdHelper();
+        { 
+            pbSetup.Maximum = 100; 
             //D:\\web\\BeiMaiProject\\beimai5.0\\Web\\BeiMai.WebApp\\BeiMai.WebApp\\bin
             string sPath = AppDomain.CurrentDomain.BaseDirectory;
             //SC Delete MongoDB  --删除服务 
@@ -59,11 +53,20 @@ sql_mode=NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES
 
             string sPort = txtPort.Text.Trim();
 
-            if (string.IsNullOrEmpty(sPort) || sPort.Length < 4 || !XS.Core.XsUtils.IsNumeric(sPort))
+            if (string.IsNullOrEmpty(sPort) || sPort.Length < 4 || !Tools.IsNumeric(sPort))
             {
                 MessageBox.Show("请输入正确的端口,端口为4位到5位的数字！");
                 return;
             }
+
+            bool paortUsed = Tools.portInUse(Tools.StrToInt(sPort));
+
+            if (paortUsed)
+            {
+                MessageBox.Show($"端口{sPort}已经其他程序占用！");
+                return;
+            }
+
 
             string basedir = string.Concat(sPath, "\\db");
 
@@ -81,25 +84,36 @@ sql_mode=NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES
             }
             btnSetup.Enabled = false;
             btnSetup.Text = "开始安装...";
-            if (cmd.CheckService("MySQL"))
+            if (Tools.CheckService("MySQL"))
             {
-                bool isok = WinformTools.ConfirmDialog("已经安装了MYSQL,是否删除，确认将删除,结束后再重试安装！");
+                bool isok = Tools.ConfirmDialog("已经安装了MYSQL,是否删除，确认将删除,结束后再重试安装！");
                 if (isok)
                 {
                     btnSetup.Text = "正在删除旧服务...";
                     Thread threadCpuInfo2 = new Thread(() =>
                     {
 
-                        cmd.RunCmd("net stop MySQL");
-                        Thread.Sleep(30000);
-                        cmd.RunCmd("SC Delete MySQL");
-                        Thread.Sleep(3000);
-                        Action<int> action = (data) =>
+                       bool isRuned = Tools.RunCmd("net stop MySQL");
+                        if (isRuned)
                         {
-                            btnSetup.Enabled = true;
-                            btnSetup.Text = "原服务已经删除，请点击重试安装";
-                        };
-                        Invoke(action, 1);
+                            isRuned = Tools.RunCmd("SC Delete MySQL");
+                            if (isRuned)
+                            {
+                                Action<int> action = (data) =>
+                                {
+                                    btnSetup.Enabled = true;
+                                    btnSetup.Text = "原服务已经删除，请点击重试安装";
+                                };
+                                Invoke(action, 1);
+                            }
+                            else
+                            {
+                                MessageBox.Show("旧服务删除失败");
+                            }
+                           
+
+                        }
+                            
 
                     });
 
@@ -118,7 +132,7 @@ sql_mode=NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES
 
             if(FObject.IsExist(datadir, FsoMethod.Folder))
             {
-                bool isok = WinformTools.ConfirmDialog("当前程序下已经存在数据库数据，是否要先删除！");
+                bool isok = Tools.ConfirmDialog("当前程序下已经存在数据库数据，是否要先删除！");
                 if (isok)
                 {
                     string dataPath = string.Concat(sPath, "\\db\\data");
@@ -141,11 +155,10 @@ sql_mode=NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES
 
             mysqlSetting = mysqlSetting.Replace("#端口#", sPort).Replace("#数据库程序目录#", basedir).Replace("#数据存储目录#", datadir).Replace("#服务路径#",WinMySQLAdminPath);
 
-            string dbSettingFile = string.Concat(sPath, "\\db\\bin\\my.ini");
+            string dbSettingFile = string.Concat(sPath, "\\db\\my.ini");
 
-            FObject.WriteFileUtf8(dbSettingFile, mysqlSetting);
+            FObject.WriteFile(dbSettingFile, mysqlSetting);
 
-             
 
             Thread threadCpuInfo = new Thread(() =>
             {
@@ -161,35 +174,54 @@ sql_mode=NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES
                 //cmds.Add(@"mysql -uroot -p");
                 //cmds.Add(sPassWord);
                 //cmds.Add(sPassWord);
-                cmd.RunCmd(cmds);
-                Thread.Sleep(50000);
-                cmds.Clear();
-                cmds.Add(string.Format("cd {0}", sPan));
-                cmds.Add(string.Format(@"cd {0}db\bin", sPath));
-                cmds.Add(@"mysqld --install");
-                cmd.RunCmd(cmds);
-                Thread.Sleep(30000);
-                cmds.Clear();
-                cmds.Add(string.Format("cd {0}", sPan));
-                cmds.Add(string.Format(@"cd {0}db\bin", sPath));
-                cmds.Add(@"net start mysql");
-                cmd.RunCmd(cmds);
-                Thread.Sleep(30000);
-                cmds.Clear();
-                cmds.Add(string.Format("cd {0}", sPan));
-                cmds.Add(string.Format(@"cd {0}db\bin", sPath));
-                cmds.Add(string.Format(@"mysqladmin -u root password {0}", sPassWord));
-                //cmds.Add(sPassWord);
-                //cmds.Add(sPassWord);
-                cmd.RunCmd(cmds);
-
-                Thread.Sleep(30000);
-                Action<int> action = (data) =>
+                bool isruned = Tools.RunCmd(cmds);
+                if (isruned)
                 {
-                    btnSetup.Enabled = true;
-                    btnSetup.Text = "开始安装";
-                };
-                Invoke(action,1);
+                    Action<int> proce_update = (data) =>
+                    {
+                        pbSetup.Value = data;
+                    };
+                    Invoke(proce_update, 20);
+                    
+                    //Thread.Sleep(50000);
+                    cmds.Clear();
+                    cmds.Add(string.Format("cd {0}", sPan));
+                    cmds.Add(string.Format(@"cd {0}db\bin", sPath));
+                    cmds.Add(@"mysqld --install");
+                    isruned = Tools.RunCmd(cmds);
+                    if (isruned)
+                    {
+                        Invoke(proce_update, 50); 
+                        cmds.Clear();
+                        cmds.Add(string.Format("cd {0}", sPan));
+                        cmds.Add(string.Format(@"cd {0}db\bin", sPath));
+                        cmds.Add(@"net start mysql");
+                        isruned = Tools.RunCmd(cmds);
+                        if (isruned)
+                        {
+                            Invoke(proce_update, 70);
+                            cmds.Clear();
+                            cmds.Add(string.Format("cd {0}", sPan));
+                            cmds.Add(string.Format(@"cd {0}db\bin", sPath));
+                            cmds.Add(string.Format(@"mysqladmin -u root password {0}", sPassWord));
+                            //cmds.Add(sPassWord);
+                            //cmds.Add(sPassWord);
+                            isruned = Tools.RunCmd(cmds);
+                            if(isruned)
+                            {
+                                Invoke(proce_update, 100);
+                                MessageBox.Show("安装完成!");
+                                Action<int> action = (data) =>
+                                {
+                                    btnSetup.Enabled = true;
+                                    btnSetup.Text = "开始安装";
+                                };
+                                Invoke(action, 1);
+                            }
+                        }
+                    }
+                } 
+                
             
                 
             });
@@ -197,56 +229,46 @@ sql_mode=NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES
 
             threadCpuInfo.Start();
 
-        }
+        } 
+        
+        
 
-        private void btnSetPrivateKey_Click(object sender, EventArgs e)
+        private void lb_del_mysql_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            string Key = "coin2018";
-            try
+            bool isok = Tools.ConfirmDialog("确定要删除MYSQL的服务吗！");
+            if (isok)
             {
-                var oldPwd = txtPass.Text.Trim();
-                if (!string.IsNullOrEmpty(oldPwd))
+                lb_del_mysql.Enabled = false;
+                Thread threadCpuInfo2 = new Thread(() =>
                 {
-                    //var enPwd = GetEncrypt(oldPwd);
-                    var enPwd = XS.Core.XsUtils.MD5(string.Concat(oldPwd, Key));
-                    SetConfigValue("Password", enPwd);
 
-                    MessageBox.Show("加密完成");
-                    //var testPwd = GetDecrypt(enPwd);
-                }
-            }
-            catch (Exception exception)
-            {
-                Console.WriteLine(exception);
-                throw;
-            }
-        }
+                    bool isRuned = Tools.RunCmd("net stop MySQL");
+                    if (isRuned)
+                    {
+                        isRuned = Tools.RunCmd("SC Delete MySQL");
+                        if (isRuned)
+                        {
+                            Action<int> action = (data) =>
+                            {
+                                lb_del_mysql.Enabled = true;
+                                lb_del_mysql.Text = "MYSQL已经删除";
+                            };
+                            Invoke(action, 1);
+                        }
+                        else
+                        {
+                            MessageBox.Show("旧服务删除失败");
+                        }
 
-        /// <summary>
-        /// 修改AppSettings中配置
-        /// </summary>
-        /// <param name="key">key值</param>
-        /// <param name="value">相应值</param>
-        private bool SetConfigValue(string key, string value)
-        {
-            try
-            {
-                var map = new ExeConfigurationFileMap();
-                map.ExeConfigFilename = @"ColdWallet.exe.Config";
 
-                Configuration config = ConfigurationManager.OpenMappedExeConfiguration(map, ConfigurationUserLevel.None);
-                if (config.AppSettings.Settings[key] != null)
-                    config.AppSettings.Settings[key].Value = value;
-                else
-                    config.AppSettings.Settings.Add(key, value);
-                config.Save(ConfigurationSaveMode.Modified);
-                ConfigurationManager.RefreshSection("appSettings");
-                return true;
+                    }
+
+
+                });
+
+                threadCpuInfo2.Start();
             }
-            catch
-            {
-                return false;
-            }
+            
         }
     }
 }
